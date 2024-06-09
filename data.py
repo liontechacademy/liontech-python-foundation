900e006a26d40b75ed81485cea457e4fcccbc7ca
lion tech academy <info@liontechacademy.comn>
2024-06-09 18:52:10 -0400

    python foundation liontech

---
+++ data.py
@@ -0,0 +1,26 @@
+name   = "john"
+print (name)
+car = "honda"
+print (car)
+# the values for strings must  be in double quotes. 
+a  =  "elvis"
+print(a)
+
+Names  =  "bathelotemosbsghshshsgfajdjss2544sdjsjbcvsjd"
+print(len(Names))
+
+Country   =   "Nigeria"
+print (Country[4])  
+
+# Looping through string
+
+Name   =    "elviseeessiiissssss"
+for    e  in  "Name":
+    print(e)
+    
+    
+#  SLICING FUNCTIONS
+
+students_name   =  ("elvis,  john")
+print(students_name[:5])
+
---
+++ demo.py
@@ -0,0 +1,16 @@
+name   =    "elvis"
+print("name")
+    
+2 + 4 
+3 -  3 
+7  * 4 
+
+price    =   100 
+
+print("price")
+# white space  \ 
+#{}
+#:
+
+2024_car  =  ["", ""]
+from_cars   
